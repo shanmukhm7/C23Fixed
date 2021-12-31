61aaf81e62e3443650fb655f0994008d63c39764
shanmukhm7 <87687336+shanmukhm7@users.noreply.github.com>
2021-12-30 19:00:36 -0800

    Add files via upload

---
+++ js/ComputerArcher.js
@@ -0,0 +1,30 @@
+
+class ComputerArcher {
+    constructor(x, y, width, height) {
+      var options = {
+        //restitution: 0.8,
+        //friction: 1.0,
+        //density: 1.0,
+        isStatic: true
+      };
+      this.width = width;
+      this.height = height;
+      this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+      this.image = loadImage("./assets/computerArcher.png");
+      World.add(world, this.body);
+      Matter.Body.setAngle(this.body,PI/2)
+    }
+  
+   display() {
+      var pos = this.body.position;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+    }
+  }
+  
